91021dda1bc0487a49d11e2fc7713f0f5575d27e
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2017-09-01 11:50:54 +0530

    Create eo.c

---
+++ eo.c
@@ -0,0 +1,13 @@
+#include<stdio>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\nEnter a num");
+scanf("%d",&a);
+if(a%2==0)
+printf("\nIt is even");
+else
+printf("\nIt is odd");
+getch();
+}
