5c1794249f8e34ee275d74c4d46bd9f4eb3480d7
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2017-09-01 11:42:23 +0530

    Create p.c

---
+++ p.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\nEnter a number...");
+scanf("%d",&a);
+if(a<0)
+{
+printf("\nThe entered number is negative");
+}
+else
+{
+printf("\nThe entered number is positive");
+}
+getch();
+}
