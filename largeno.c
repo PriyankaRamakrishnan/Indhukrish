e9752a9f2c570303721a69b08c921a648c70fa6e
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2017-09-12 16:42:36 +0530

    Create largeno.c

---
+++ largeno.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\nEnter a");
+scanf("%d",&a);
+printf("\nenter b");
+scanf("%d",&b);
+printf("\nenter c");
+scanf("%d",&c);
+if(a>b&&a>c)
+printf("\a is big");
+elseif(b>a&&b>c)
+printf("\b is big");
+else
+printf("\c is big");
+getch();
+}
