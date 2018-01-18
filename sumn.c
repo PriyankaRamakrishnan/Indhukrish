5e6aefbcd744282e73353c8ddf83fac526881e1c
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-01-18 15:58:29 +0530

    Create sumn.c

---
+++ sumn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,sum=0;
+printf("\nEnter the N count:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+sum=sum+i;
+printf("sum",sum);
+getch();
+}
