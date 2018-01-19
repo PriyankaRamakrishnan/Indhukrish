990c646d739c70b185d90571026fb46dd2d2bccc
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-01-19 16:03:14 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+void main()
+{
+int a,p,result;
+printf("\nenter the num");
+scanf("%d",&a);
+printf("\nenter the power val");
+scanf("%d",&p);
+result=pow(a,p);
+printf("%d",result);
+}
