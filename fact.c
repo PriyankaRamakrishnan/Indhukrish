395c6e9b11e946d442ce8c271cceca296a7d90cc
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-01-21 11:47:07 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
- 
-int main()
+ void main()
 {
   int a, n, fact = 1;
- 
-  printf("Enter a number to calculate its factorial\n");
-  scanf("%d", &n);
- 
-  for (a = 1; a <= n; a++)
-    fact = fact * a;
- 
-  printf("Factorial of %d = %d\n", n, fact);
- 
-  return 0;
+ printf("Enter a number to calculate its factorial\n");
+ scanf("%d", &n);
+ for (a = 1; a <= n; a++)
+ fact = fact * a; 
+ printf("Factorial of %d = %d\n", n, fact);
 }
