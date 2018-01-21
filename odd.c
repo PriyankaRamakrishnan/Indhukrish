fe1fd692c008b9b4599e2c5fbf1954aabfce31ee
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-01-21 09:31:44 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int in1,in2,i;
+printf("\nenter the interval points");
+scanf("%d%d",&in1,&in2);
+for(i=int1;i<=int2;i++)
+{
+if(i/2!=0)
+printf("\n%d",i);
+}
+getch();
+}
