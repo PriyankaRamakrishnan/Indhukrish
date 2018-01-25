8f5af4deb3f745447122a368aa23cd41f79e4714
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-01-25 16:36:59 +0530

    Update largeinarray.c

--- largeinarray.c
+++ largeinarray.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int a[100],n,i,largevalue;
-printf(""\nenter a N limit");
-scanf("%d",&n);
-for(a[i]=1;a[i]<=n;a[i]++)
-printf("\n%d",a[i]);
-for(a[i]=1;a[i]<=n;a[i]++)
-scanf("%d",&a[1]);
-largevalue=a[1];
-for(a[i]=2;a[i]<=n;a[i]++)
-{
-if(largevalue < a[i])
-largevalue=a[i];
-}
+  int a[50], N, i, largest;
+    printf("\n Enter the size of the array: ");
+    scanf("%d", &N);
+    printf("\n Enter elements of  the array: ");
+    for (i = 0; i < N; i++)
+        scanf("%d", &a[i]);
+    largest = a[0];
+    for (i = 1; i < N; i++)
+    {
+        if (largest < a[i])
+            largest = a[i];
+    }
+    printf("\n largest element present in the given array is : %d", largest);
+    return 0;
 }
