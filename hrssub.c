98067f42f117425c7ccf2c5f78757399851cb418
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-02 09:48:09 +0000

    Create hrssub.c

---
+++ hrssub.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int hrs1,mins1,hrs2,mins2,hr1,hr2,tot_mins1,tot_min2,tot_mins,hr,min;
+printf("\nenter the time in hh:mm :");
+scanf("%d%d",&hrs1,&mins);
+printf("\nEnter the time in hh:mm :");
+scanf("%d%d",&hrs2,mins2);
+hr1=(hrs1*60);
+tot_mins1=hr1+mins1;
+hr2=(hrs2*60);
+tot_mins2=hr2+mins2;
+tot_mins=tot_mins1-tot_mins2;
+hr=tot_mins/60;
+min=tot_mins%60;
+printf("%d:%d",hr,min);
+}
