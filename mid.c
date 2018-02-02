e49bb3459c255a40e47135a70d549a45aded038d
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-02 08:55:51 +0000

    Create mid.c

---
+++ mid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define max_size 100
+int main()
+{
+    int arr[max_size];
+    int size,i,j,temp,mid;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(i=0; i<size; i++)
+  {
+        for(j=i+1; j<size; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                temp     = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("\nElements of array in ascending order: ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d", arr[i]);
+    }
+mid=size/2;
+printf("\nThe median element is :%d",arr[mid]);
+    return 0;
+    }
