7a776a92d469aca5a953a9aa78e69625b32a9e69
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-08 15:09:25 +0530

    Create stringnum.c

---
+++ stringnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int K,i;
+  char S[100];
+  printf("\nenter any string and num :");
+  scanf("%s%d",&S,&K);
+  for(i=0;i<K;i++)
+  {
+    printf("\n%s",S);
+  }
+}
