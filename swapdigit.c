b54b76e47d458effd34b3dc915080e9a31fd6d2c
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-08 14:14:08 +0530

    Create swapdigit.c

---
+++ swapdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2,ten1,ten2,one1,one2;
+  printf("\nEnter any 2 num :");
+  scanf("%d%d",&num1,&num2);
+  one1=num1%10;
+  one2=num2%10;
+  ten1=num1/10;
+  ten2=num2/10;
+  num1=(one1*10)+ten1;
+  num2=(one2*10)+ten2;
+  printf("\n%d %d",num1,num2);
+  
+}
