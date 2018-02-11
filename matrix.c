c6880c34d0fe51705e8df2c2a006701057a91311
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-11 06:00:19 +0000

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void main()
+{
+ int a[10][10],i,j,max,r,c;
+ printf("Priyanka R");
+ printf("\n922515104067");
+ printf("\nenter the row and column");
+ scanf("%d%d",&r,&c);
+ for(i=0;i<r;i++)
+ {
+   for(j=0;j<c;j++)
+   {
+      scanf("%d",&a[i][j]);
+   }
+ }
+ for(i=0;i<r;i++)
+ {
+   for(j=0;j<c;j++)
+   {
+     printf("\t");
+      printf("%d",a[i][j]);
+   }
+ 
+  printf("\n");
+ }
+  max=a[0][0];
+  for(i=1;i<r;i++)
+{
+  for(j=1;i<c;i++)
+  {
+    if(max<a[i][j])
+    max=a[i][j];
+  }
+}  
+ 
+  printf("\nMax value :%d",max);
+}
