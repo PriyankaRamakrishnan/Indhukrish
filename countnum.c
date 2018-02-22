507118b0dabc43651012f4f49c7665b1a2e041cf
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-22 15:54:27 +0530

    Create countnum.c

---
+++ countnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int count=0,n;
+printf("\nEnter the num");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    n=n/10;
+    ++count;
+  }
+  printf("\ncount %d",count);
+}
