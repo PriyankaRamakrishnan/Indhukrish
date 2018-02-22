a24b1b40fc4bd7105c161302555102ccf2d5dd8d
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-02-22 16:50:17 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int num;
+    int temp,f;
+     
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+     temp=num;
+    f=0;
+    while(temp!=1)
+    {
+        if(temp%2!=0){
+            f=1;
+            break;
+        }
+        temp=temp/2;
+    }
+  
+    if(f==0)
+        printf("%d is a number that is the power of 2.",num);
+    else
+        printf("%d is not the power of 2.",num);
+      
+    return 0;
+}
