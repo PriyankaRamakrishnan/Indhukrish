8e9e36f8eb7fd4dacfb135e37a92f5b6cd1d2795
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-03-01 16:54:45 +0530

    Create large10.c

---
+++ large10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,num[10],largenum;
+for(i=0;i<=9;i++)
+{
+printf("\nEnter any 10 num :");
+scanf("%d",&num[i]);
+}
+largenum=num[0];
+for(i=1;i<=9;i++)
+{
+if(largenum<num[i])
+largenum=num[i];
+}
+printf("\nmaximum among ten num :%d",num[i]);
+}
