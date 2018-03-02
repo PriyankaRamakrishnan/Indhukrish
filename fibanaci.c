22be7c9cc8caeda92526d19e42401b853008320c
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-03-02 15:51:30 +0530

    Update fibanaci.c

--- fibanaci.c
+++ fibanaci.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int n,n1=0,n2=1,fib,i;
+int n,n1=1,n2=1,fib,i;
   printf("\nEnter a limit for fibanacci series :");
   scanf("%d",&n);
   printf("\nFibanaci series is :");
-  for(i=0;i<=n;i++)
+  for(i=0;i<n;i++)
   {
     printf("%d",n1);
     fib=n1+n2;
