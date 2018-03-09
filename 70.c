caa002b5b89c433bf1e2c528fee0cc49865309ce
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-03-09 16:18:22 +0530

    Update 70.c

--- 70.c
+++ 70.c
@@ -1,41 +1,20 @@
 #include<stdio.h>
 void main()
 {
-int a;
+int a,i;
 printf("\nEnter the num :");
 scanf("%d",&a);
-for(i=a+1;i<=a+3;i++)
-{
-if(i%2==0)
-printf("\n Nearest greater power of 2 for given number %d : %d",a,i);
-}
-}
-#include <stdio.h>
- 
-int main()
-{
-    int num;
-    int tempNum,flag;
-     
-    printf("Enter an integer number: ");
-    scanf("%d",&num);
-     
-    tempNum=num;
-    flag=0;
-    /*check power of two*/
-    while(tempNum!=1)
-    {
-        if(tempNum%2!=0){
-            flag=1;
-            break;
-        }
-        tempNum=tempNum/2;
-    }
-  
-    if(flag==0)
-        printf("%d is a number that is the power of 2.",num);
-    else
-        printf("%d is not the power of 2.",num);
-      
-    return 0;
+ if(a%2==0)
+ {
+   printf("\nNearest power of 2 :%d",a*2);
+ }
+ else
+ {
+ for(i=a+1;i<=1000;i++)
+ {
+ if(i%2==0)
+  printf("\nNearest power of 2 :%d",i*2);
+  break;
+ }
+ }
 }
