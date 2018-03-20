98c98fbce93b0aa1f9422d6ab0af9ad2447eb200
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-03-20 15:54:29 +0530

    Create perfect square.c

---
+++ perfect square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+printf("\nEnter any num for product :");
+scanf("%d %d",&a,&b);
+c=a*b;
+if(a==b||(pow(a,2)==c))
+printf("\nYes");
+else
+printf("\nNo");
+}
