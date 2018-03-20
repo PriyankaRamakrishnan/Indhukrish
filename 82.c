671560176a3aa1deb44cfed455650fd669d7aa8a
PriyankaRamakrishnan <30828497+PriyankaRamakrishnan@users.noreply.github.com>
2018-03-20 16:34:01 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+float L,B,area;
+printf("\nEnter the length,breadth of the farm :");
+scanf("%f %f",&L,&B);
+area=L*B;
+printf("\nThe area of the farm is %0.5f",area);
+}
